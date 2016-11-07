4221fdaa2a0eed2e46fcf324a92cb25e8e656385
Steven Bateman <BatemanVO@gmail.com>
2016-11-07 15:06:37 -0800

    Wrote out solutions to the errors created.

--- starter-code/scripts/models/article.js
+++ starter-code/scripts/models/article.js
@@ -1,5 +1,3 @@
-// TODO: Wrap the entire contents of this file in an IIFE.
-// Pass in to the IIFE a module, upon which objects can be attached for later access.
 (function(module){
   function Article (opts) {
     for (key in opts) {
@@ -16,9 +14,6 @@
   };
 
   Article.loadAll = function(dataWePassIn) {
-    /* NOTE: the original forEach code should be refactored
-       using `.map()` -  since what we are trying to accomplish is the
-       transformation of one collection into another. */
     Article.allArticles = dataWePassIn.sort(function(a,b) {
       return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
     }).map(function(ele) {
@@ -26,11 +21,6 @@
     });
   };
 
-  /* NOTE: Refactoring the Article.fetchAll method, it now accepts a parameter
-      that will execute once the loading of articles is done. We do this because
-      we might want to call other view functions, and not just renderIndexPage();
-      Now instead of calling articleView.renderIndexPage(), we can call
-      whatever we pass in! */
   Article.fetchAll = function(next) {
     if (localStorage.hackerIpsum) {
       $.ajax({
@@ -39,18 +29,15 @@
         success: function(data, message, xhr) {
           var eTag = xhr.getResponseHeader('eTag');
           if (!localStorage.eTag || eTag !== localStorage.eTag) {
-            // DONE: pass 'next' into Article.getAll();
-            Article.getAll(next);
+            Article.getAll(); //TODO: next needs to be passed into Article.getAll();
           } else {
             Article.loadAll(JSON.parse(localStorage.hackerIpsum));
-            // DONE: invoke next
             next();
           }
         }
       });
     } else {
-      // DONE: pass 'next' into getAll();
-      Article.getAll(next);
+      Article.getAll(); //TODO: next needs to be passed into Article.getAll();
     }
   };
 
@@ -59,29 +46,20 @@
       localStorage.eTag = xhr.getResponseHeader('eTag');
       Article.loadAll(responseData);
       localStorage.hackerIpsum = JSON.stringify(responseData);
-      // DONE: invoke next!
       next();
     });
   };
 
-  /* DONE: Chain together a `map` and a `reduce` call to get a rough count of
-      all words in all articles. */
   Article.numWordsAll = function() {
     return Article.allArticles.map(function(article) {
-      //DONE: Grab the word count from each article body.
       return article.body.split(' ').length;
-    })
-    // DONE: complete this reduce to get a grand total word count
+    }); //This semicolon needs to be removed
     .reduce(function(current, next, idx, array) {
       return (current + next);
     });
   };
 
-  /* DONE: Chain together a `map` and a `reduce` call to
-            produce an array of *unique* author names. */
   Article.allAuthors = function() {
-    //return       TODO: map our collection
-      //return    TODO: return just the author names
     return Article.allArticles.map(function(article){
       return article.author;
     }).reduce(function(acc, next, idx, array){
@@ -90,18 +68,12 @@
       };
       return acc;
     }, []);
-    /* DONE: For our `reduce` that we'll chain here -- since we are trying to
-        return an array, we'll need to specify an accumulator type...
-        What data type should this accumulator be and where is it placed? */
   };
 
   Article.numWordsByAuthor = function() {
-    /* DONE: Transform each author element into an object with 2 properties:
-        One for the author's name, and one for the total number of words across
-        the matching articles written by the specified author. */
-    return Article.allAuthors().map(function(author) {
+    Article.allAuthors().map(function(author) { //A return needs to be added to the beginning of this line
       return {
-        name: author,
+        name: author; //This semicolon needs to be changed to a comma
         numWords: Article.allArticles.filter(function(curArticle) {
           return curArticle.author === author;
         }).map(function(article) {
